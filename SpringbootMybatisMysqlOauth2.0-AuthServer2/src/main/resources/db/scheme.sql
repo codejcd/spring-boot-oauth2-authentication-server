1f47a0de484fa6335c400254ea7695478d715c32
codejcd <codejcd@gmail.com>
2019-08-18 19:16:23 +0900

    db scheme

---
+++ SpringbootMybatisMysqlOauth2.0-AuthServer2/src/main/resources/db/scheme.sql
@@ -0,0 +1,28 @@
+CREATE TABLE `oauth_client` (
+	`client_seq` INT(11) NOT NULL AUTO_INCREMENT,
+	`client_id` VARCHAR(256) NOT NULL,
+	`client_secret` VARCHAR(256) NOT NULL,
+	`status` VARCHAR(50) NOT NULL DEFAULT 'A',
+	`reg_date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (`client_seq`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+AUTO_INCREMENT=5
+;
+
+CREATE TABLE `user` (
+	`user_seq` INT(11) NOT NULL AUTO_INCREMENT,
+	`user_id` VARCHAR(100) NULL DEFAULT NULL,
+	`password` VARCHAR(256) NOT NULL,
+	`name` VARCHAR(100) NOT NULL,
+	`status` VARCHAR(100) NOT NULL DEFAULT 'A',
+	`reg_date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (`user_seq`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+AUTO_INCREMENT=3
+;
+
+
